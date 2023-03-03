7916040038cb2d1529014c013da4ba42b2d5bf35
Jonay <alu0101347141@ull.edu.es>
2023-03-03 10:19:14 +0000

    Ajustes iniciales para trabajo en portátil

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>A Sandra no le gusta mi antivirus</Text>
+      <Text>Lorem Ipsum</Text>
       <StatusBar style="auto" />
     </View>
   );
