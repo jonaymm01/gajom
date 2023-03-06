f9d4cb39532a8f81243a40c30b190d8651cfabdf
Jonay <alu0101347141@ull.edu.es>
2023-03-06 19:35:34 +0000

    Nueva versión de página principal. Tres Botones planteado

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -3,31 +3,42 @@ import { StyleSheet, Text, View, Image, Button, Alert, TouchableOpacity } from '
 
 export function Main() {
   const [count, setCount] = useState(0);
-  const onPress = () => alert('A');
+  const onPressSupport = () => alert('olacrak');
+  const onPressUser = () => alert('olacrok');
+  const onPressLogopedazo = () => alert('olacrek');
+
     return (
-      <View style={styles.container}>
-      <View style={styles.content}>
-        <Image source={require('../assets/logo/logo_gajom.png')} 
-        style={{width: 150, height: 150}}/>
-      </View>
-      <View style={styles.content}>
-        <Text>LOGOPEDAZO</Text>
-      </View>
-      </View>
+    <View style={[ styles.container, {flexDirection: 'column', padding: 0}]}>
+    <View style={[ styles.container, {flexDirection: 'row', padding: 0}]}>
+      <TouchableOpacity  style={[styles.cuadritofeo, {backgroundColor: '#AC3C60', borderEndWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressSupport} />
+      <TouchableOpacity  style={[styles.cuadritofeo, {backgroundColor: '#AC3C60', borderLeftWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressUser} />
+    </View>
+    <TouchableOpacity  style={[styles.cuadritofeo, {backgroundColor: '#AC3C60', borderTopWidth: 2.5}]} onPress={onPressLogopedazo}/>
+    </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      padding: 5,
     },
-    content: {
+    cuadritofeo: {
       flex: 1, 
-      alignItems: 'center', 
-      justifyContent: 'center'
+      borderWidth: 5,
+      borderColor: 'white'
+
+    },
+    logopedazo_container: {
+      flex: 1, 
+      backgroundColor: '#000',
+      fontSize: 16,
+      width: 30
+    },
+    logopedazo_texto: {
+      flex: 1, 
+      color: '#fff',
+      fontSize: 40,
     },
     /*buttons_container: {
       flex: 1, 
