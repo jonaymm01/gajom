0a906e13a95dcce69b8f55f54a68929c7500e583
Jonay <alu0101347141@ull.edu.es>
2023-03-07 18:41:12 +0000

    Terminada primera versión de página principal

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -1,25 +1,38 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Image, Button, Alert, TouchableOpacity } from 'react-native';
+import React, {useState, useCallback} from 'react';
+import { StyleSheet, Text, View, Image, Button, Alert, TouchableOpacity, Linking} from 'react-native';
 import { GamesStackNavigator, MainStackNavigator, TalkerStackNavigator } from "../components/StackNavigator";
 
 
 export function Main({ navigation }) {
   const onPressSupport = () => navigation.navigate("Soporte")
   const onPressUser = () => navigation.navigate("Mi perfil")
-  const onPressLogopedazo = () => alert('Esto es un Logopedazo');
+  const logopedazo_content = "La afonía es perder por completo la voz. La disfonía, pérdida parcial. ¿Voz de camionero post-fiesta? No estás afónico... ¡estás disfónico!"
+  const logopedazo_url = 'https://es.wikipedia.org/wiki/Disfon%C3%ADa';
+  const onPressLogopedazo = () => Linking.openURL(logopedazo_url);
 
     return (
     <View style={[ styles.container, {flexDirection: 'column', padding: 0}]}>
-    <View style={[ styles.container, {flexDirection: 'row', padding: 0}]}>
-      <TouchableOpacity  style={[styles.button, {backgroundColor: '#AC3C60', borderEndWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressSupport}>
+      <View style={[ styles.container, {flexDirection: 'row', padding: 0}]}>
+        <TouchableOpacity  style={[styles.button, {backgroundColor: '#AC3C60', borderEndWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressSupport}>
+          <View style={styles.button_container}>
+            <Text style={styles.button_text}>Centro de Ayuda</Text>
+            <Image source={require('../assets/SupportImage.png')} resizeMode='contain' style={{flex:1, marginBottom: 60}} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity  style={[styles.button, {backgroundColor: '#AC3C60', borderLeftWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressUser}>
+          <View style={styles.button_container}>
+            <Text style={styles.button_text}>Editar perfil</Text>
+            <Image source={require('../assets/UserImage.png')} resizeMode='contain' style={{flex:1, marginBottom: 60}} />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity  style={[styles.button, {backgroundColor: '#AC3C60', borderTopWidth: 2.5}]} onPress={onPressLogopedazo}>
       <View style={styles.button_container}>
-        <Image source={require('../assets/favicon.png')} resizeMode='contain' style={{flex:.2 }} />
-        <Text style={styles.button_text}>Necesito ayuda</Text>
+        <Image source={require('../assets/logo/logo_gajom.png')} resizeMode='contain' style={{flex:1, marginTop: -70, borderColor: 'blue'}} />
+        <Text style={styles.logopedazo_title}>LOGOPEDAZO DEL DÍA</Text>
+        <Text style={styles.logopedazo_text}>{logopedazo_content}</Text>
       </View>
       </TouchableOpacity>
-      <TouchableOpacity  style={[styles.button, {backgroundColor: '#AC3C60', borderLeftWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressUser} />
-    </View>
-    <TouchableOpacity  style={[styles.button, {backgroundColor: '#AC3C60', borderTopWidth: 2.5}]} onPress={onPressLogopedazo}/>
     </View>
     );
 }
@@ -41,10 +54,21 @@ const styles = StyleSheet.create({
       fontSize: 16,
       width: 30
     },
-    logopedazo_texto: {
+    logopedazo_title: {
       flex: 1, 
       color: '#fff',
-      fontSize: 40,
+      fontSize: 30,
+      justifyContent: 'center',
+      fontWeight: 'bold',
+      fontStyle: 'italic',
+      lineHeight: 80
+    },
+    logopedazo_text: {
+      flex: 1, 
+      color: '#fff',
+      fontSize: 20,
+      justifyContent: 'center',
+      marginTop: -60
     },
     button_container: {
       flex: 1, 
@@ -61,9 +85,10 @@ const styles = StyleSheet.create({
       marginTop: 10
     },
     button_text: {
+      flex: 1,
       color: 'white',
       fontWeight: 'bold',
       fontSize: 20,
-      lineHeight: 80
+      lineHeight: 120,
     },
 });
