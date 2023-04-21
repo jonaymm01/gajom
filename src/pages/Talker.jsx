656eb7bab21dad57830448065f6c20c50d6d3b56
JonayMendez <jonay.mendez@holasoft.es>
2023-04-21 15:52:40 +0100

    Implementado menú principal de Comunicador

--- src/pages/Talker.jsx
+++ src/pages/Talker.jsx
@@ -1,14 +1,54 @@
-import {Text, View} from 'react-native';
-import {styles} from '../styles/styles';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {styles, palette} from '../styles/styles';
+
 
 /**
  * Método para renderizar la página del Comunicador.
  * @return {JSX.Element}
  */
-export function Talker() {
+export function Talker({navigation}) {
+  const onPressFQA = () => navigation.navigate('FQA');
+  const onPressContact = () => navigation.navigate('Contact');
+
   return (
-    <View style={styles.blank_background}>
-      <Text>Menú de Comunicador</Text>
-    </View>
+    <>
+
+      <View style={[styles.container, {flexDirection: 'column', padding: 0}]}>
+        <TouchableOpacity style={[styles.button, {backgroundColor: palette.red}]} onPress={onPressFQA}>
+          <View style={styles.button_container}>
+            <Text style={talkerStyles.button_text}>PICTOGRAMAS</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, {backgroundColor: palette.red}]} onPress={onPressContact}>
+          <View style={styles.button_container}>
+            <Text style={talkerStyles.button_text}>TABS</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, {backgroundColor: palette.red}]} onPress={onPressContact}>
+          <View style={styles.button_container}>
+            <Text style={talkerStyles.button_text}>PREGUNTADOR</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
+
+const talkerStyles = StyleSheet.create({
+  box: {
+    alignItems: 'center',
+    height: 100,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'justify',
+    color: palette.violet,
+    lineHeight: 100,
+  },
+  button_text: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+});
