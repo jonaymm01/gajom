d5cc9df505b3b152b697225569d0e2e4169f622e
JonayMendez <jonay.mendez@holasoft.es>
2023-04-24 23:50:13 +0100

    Añadido TexToSpeech en TAPs

--- src/components/TOList.js
+++ src/components/TOList.js
@@ -1,15 +1,23 @@
 import React, {ReactNode} from 'react';
 import {TouchableOpacity, StyleSheet, View, Text} from 'react-native';
 import {styles, palette} from '../styles/styles';
-
+import * as Speech from 'expo-speech';
 
 /**
  * Método para renderizar lista de botones
  * @return {JSX.Element}
  */
 export default function TOList({...props}) {
+  /**
+   * Método para activar la lectura de texto del TAP
+   * @param {string} text
+   */
+  const speak = (text) => {
+    Speech.speak(text);
+  };
+
   const taplist = props.list.map((tap) =>
-    <TouchableOpacity style={[styles.button, {backgroundColor: tap.color}]} onPress={()=>{}}>
+    <TouchableOpacity style={[styles.button, {backgroundColor: tap.color}]} onPress={()=>speak(tap.text)}>
       <View style={styles.button_container}>
         <Text style={talkerStyles.button_text}>{tap.text}</Text>
       </View>
