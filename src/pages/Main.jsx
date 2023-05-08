00d3cc3903c0f798b9c107782631d83bbdaa3156
JonayMendez <jonay.mendez@holasoft.es>
2023-05-08 01:53:19 +0100

    Cambio de diseño Inicio

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -18,16 +18,16 @@ export function Main({navigation}) {
   return (
     <View style={[styles.container, {flexDirection: 'column', padding: 0}]}>
       <View style={[styles.container, {flexDirection: 'row', padding: 0}]}>
-        <TouchableOpacity style={[styles.button, {backgroundColor: palette.red, borderEndWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressFQA}>
-          <View style={styles.button_container}>
-            <Text style={styles.button_text}>Dudas</Text>
-            <Image source={require('../../assets/fqa_icon.png')} resizeMode='contain' style={{flex: 1, marginBottom: 80}} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, {backgroundColor: palette.red, borderLeftWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressContact}>
+        <TouchableOpacity style={[styles.button, {backgroundColor: palette.red, borderEndWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressContact}>
           <View style={styles.button_container}>
             <Text style={styles.button_text}>Contacto</Text>
             <Image source={require('../../assets/contact_icon.png')} resizeMode='contain' style={{flex: 1, marginBottom: 80, width: 100}} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, {backgroundColor: palette.red, borderLeftWidth: 2.5, borderBottomWidth: 2.5}]} onPress={onPressFQA}>
+          <View style={styles.button_container}>
+            <Text style={styles.button_text}>Dudas</Text>
+            <Image source={require('../../assets/fqa_icon.png')} resizeMode='contain' style={{flex: 1, marginBottom: 80, width: 100}} />
           </View>
         </TouchableOpacity>
       </View>
